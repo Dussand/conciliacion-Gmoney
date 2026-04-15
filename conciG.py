84c05669f533ca95f590475b7d8c4f66b0f4ce5c
Dussand <dm1020000@gmail.com>
2026-04-14 20:29:04 -0500

    fix: leer PPY_external_id as str

--- conciG.py
+++ conciG.py
@@ -587,7 +587,7 @@ elif tipo_conciliacion == "Conciliacion PayIns - Diaria":
             dfs = []
 
             for archivo in archivo_metabase:
-                df_temp = pd.read_excel(archivo, dtype={'PC_create_date_GMT_Peru': str})
+                df_temp = pd.read_excel(archivo, dtype={'PPY_external_id': str})
                 dfs.append(df_temp)
             
             df_metabase = pd.concat(dfs, ignore_index=True)
